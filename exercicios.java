84533c1c56ed44e06c1f652502104201ee97e4de
Geovana Maria <102238021+GeovanaMaria@users.noreply.github.com>
2022-12-01 11:04:26 -0300

    Update exercicios.java

--- exercicios.java
+++ exercicios.java
@@ -15,7 +15,7 @@ public class exercicios {
 
 /*-----------------------------------------------------------------------------------------------------------------*/
 
-/*01: Leia dois valores inteiros, e imprima a soma desses números
+//01: Leia dois valores inteiros, e imprima a soma desses números
 
             int A, B, X;
             System.out.println("informe dois valores: ");
@@ -25,10 +25,10 @@ public class exercicios {
             X = A+ B;
             System.out.println("x = " + X);
 
- --------------------------------------------------------------------------------------------------------------*/
+ /*--------------------------------------------------------------------------------------------------------------*/
 
 /*02: Efetue o calculo da area: (formula > area = n * (raoio * raio)/ pi = 3,14159)
-OBS: imprima o resultdo com 4 casas decimais.
+      OBS: imprima o resultdo com 4 casas decimais.*/
 
         double area, raio, pi = 3.14159;
 
@@ -39,9 +39,9 @@ OBS: imprima o resultdo com 4 casas decimais.
 
         System.out.printf("A=%.4f%n", area);
 
- ----------------------------------------------------------------------------------------------------------------*/
+ /*----------------------------------------------------------------------------------------------------------------*/
 
-/*03: Leia dois valores inteiros, calcule o produto entre os dois valores e imprima o resultado.
+//03: Leia dois valores inteiros, calcule o produto entre os dois valores e imprima o resultado.
 
         int a, b, prod;
 
@@ -53,9 +53,9 @@ OBS: imprima o resultdo com 4 casas decimais.
 
         System.out.println("PRODUTO = "+ prod);
 
------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------*/
 
-/*04: Leia duas notas do aluno, e imprima a média deles (sabendo que nota 1 tem peso 3.5 e nota2 tem peso 7.5, totalizando peso 11)
+//04: Leia duas notas do aluno, e imprima a média deles (sabendo que nota 1 tem peso 3.5 e nota 2 tem peso 7.5, totalizando peso 11)
 
             double nota1, nota2, media;
             System.out.println("informe duas notas: ");
@@ -66,10 +66,10 @@ OBS: imprima o resultdo com 4 casas decimais.
 
              System.out.printf("MEDIA = %.5f%n", media);
 
-------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------------------*/
 
 /*05: Leia 3 valores que serão representados pela nota de três alunos, calcule a média levamdo em consideração que a
-        (nota A tem peso 2, B peso 3, C peso 5).
+        (nota A tem peso 2, B peso 3, C peso 5).*/
 
 
                 System.out.println("informe três notas: ");
@@ -79,10 +79,10 @@ OBS: imprima o resultdo com 4 casas decimais.
                      double Media = (a * 2 + b * 3 + c * 5) /10;
 
                      System.out.printf("MEDIA = %.1f%n", Media);
------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------*/
 
-/*06: Leia 3 valores inteiros, calcule e motsre a diferença do produto A e B, do produto C e D, use a
-        seguinte formula DIFERENCA = (A * B - C * D).
+/*06: Leia 3 valores inteiros, calcule e mostre a diferença do produto A e B, do produto C e D, use a
+        seguinte fórmula DIFERENCA = (A * B - C * D).*/
 
                 int A,B,C,D, DIFERENCA;
                 System.out.println("Informe quatro numeros inteiros: ");
@@ -94,10 +94,10 @@ OBS: imprima o resultdo com 4 casas decimais.
                 DIFERENCA = A * B - C * D;
 
                 System.out.println("DIFERENCA = " +DIFERENCA);
--------------------------------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------------------------*/
 
 /*07: Leia o id de um funcionário, suas horas trabalhadas, valor que recebe por hora, e calcule o valor de
-        seu salário.
+        seu salário.*/
 
                 int num, horaTrabalho;
                 double ganhoPorHora, salario;
@@ -113,11 +113,11 @@ OBS: imprima o resultdo com 4 casas decimais.
 
                 System.out.println("NUMBER = "+num);
                 System.out.printf("SALARY = U$ %.2f%n", salario);
--------------------------------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------------------------*/
 
-/*08: Leia o nome de um vendedor, seu salário fixo, e o total d evendas efetuadas no mês. Leve em
+/*08: Leia o nome de um vendedor, seu salário fixo, e o total de vendas efetuadas no mês. Leve em
         consideração que esse vendedor ganha 15% de comissão sobre suas vendas realizadas. Informe o total que ele irá receber
-        (com duas casas decimais).
+        (com duas casas decimais).*/
 
                      System.out.println("Insira o nome, salario, e vendas efetuadas: ");
                      String nome = sc.next();
@@ -127,10 +127,10 @@ OBS: imprima o resultdo com 4 casas decimais.
                      double receber = sal + vendas * 0.15;
 
                      System.out.printf("TOTAL = R$ %.2f%n", receber);
--------------------------------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------------------------*/
 
 /*09:  ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça
-         2, o número de peças 2 e o valor unitário de cada peça 2, calcule e mostre o valor a ser pago.
+         2, o número de peças 2 e o valor unitário de cada peça 2, calcule e mostre o valor a ser pago.*/
 
                 int cod1, cod2, quant1, quant2;
                 double valorUni1, valorUni2, total;
@@ -147,10 +147,10 @@ OBS: imprima o resultdo com 4 casas decimais.
                 total = (quant1 * valorUni1) + (quant2 * valorUni2);
 
                 System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
--------------------------------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------------------------*/
 
-/*10: Calcule e mostre o volume d euma esfera, sendo fornecido raio (R), para calcular o volume use:  (4/3) * pi * (R * R * R)
-        Lembrando que o valor de pi é 3.14159.
+/*10: Calcule e mostre o volume de uma esfera, sendo fornecido raio (R), para calcular o volume use:  (4/3) * pi * (R * R * R).
+        Lembrando que o valor de pi é 3.14159.*/
 
         double raio, volume;
         double pi = 3.14159;
@@ -160,7 +160,7 @@ OBS: imprima o resultdo com 4 casas decimais.
         volume = 4.0/3.0 * pi * Math.pow(raio, 3.0);
 
         System.out.printf("VOLUME = %.3f\n", volume);
----------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
 
     }
 }
