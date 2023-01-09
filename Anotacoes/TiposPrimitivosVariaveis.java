1622a855c9502ad093ac096565fae982ddea8f7f
Geovana Maria <102238021+GeovanaMaria@users.noreply.github.com>
2023-01-09 15:52:59 -0300

    Add files via upload

---
+++ Anotacoes/TiposPrimitivosVariaveis.java
@@ -0,0 +1,21 @@
+package Anotacoes;
+
+public class TiposPrimitivosVariaveis {
+
+    /*
+    VARIÁVEIS: porção de memória utilizada para armazenar dados durante a execução de um programa;
+
+    TIPOS DE VARIAVÉIS:
+    String (conjunto de carateres)
+    Char (caracter)
+    Int (números inteiros)
+    Double (números reais com precisão de 10 casas após à vírgula)
+    Float (números reais com precisão de 6 casa após à vírgula)
+    Boolean (true ou false)
+
+
+     */
+
+
+
+}
