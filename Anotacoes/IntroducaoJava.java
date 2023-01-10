e99e1e880fe0ece06d2b0391c6672bdee5a6c3a3
Geovana Maria <102238021+GeovanaMaria@users.noreply.github.com>
2023-01-10 08:37:23 -0300

    Update IntroducaoJava.java

--- Anotacoes/IntroducaoJava.java
+++ Anotacoes/IntroducaoJava.java
@@ -1,7 +1,31 @@
 package Anotacoes;
 
 public class IntroducaoJava {
-    // compilacao manualmente
-    // bytecode etc
+    
+      /*
+      
+      > Java é uma linguagem sensitiva, bastante sensivel e necessita que todos os pontos,
+        chaves, entre outros estejam em seu devido lugar.
+        Java é uma linguagem que precisa ser compilada para que seja interpretada pela máquina e
+        rodada num determinado sistema operacional. Antes era necessário escrever um código especifico para
+        ser rodado em cada sistema. Para facilitar, é feito um processo mais simples, onde o programador coda
+        e faz a compilação do código, os passos são os seguintes:
 
+       > codigo fonte (codigo que o programador escreve
+       > processo de compilação (.class) > o arquivo é compilado em .class que é o código que pode ser
+         interpretado pela máquina.
+         após isso le pode ser rodsdo em qualquer sistema sem precisar escrver u codigo esecofico.
+
+        > Para fazer isso manualmente é necessário apenas pegar o código fonte, e atráves do terminal do
+          próprio computador fazer a conversão para o .class, (nesse momento o código passa de .java para .class),
+          e através disso o JVM faz o processo de executar o código na máquina virtual, sem precisar de 
+          um código especifico para cada sistema operacional.
+
+         JVM > a JVM é a máquina virtual do java, e é necessário para interpretar os códigos em java
+               e faz a conversão do bytecode para o código que vai ser executado na máquina virtual.
+         JRE > Java Runtime Environment, necessário para que o programador possa escrever códigos em java.
+         JDK > kit de desenvolvimento java, que dentro dele contém tudo que é necessario para codar na
+               linguagem java, dentro do JDK contém o JRE e JVM, é um kit java bastante completo.
+
+     */
 }
